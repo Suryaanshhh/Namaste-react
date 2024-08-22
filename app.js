cc538b87103c4fee84ab6bc439ce8488819cbd85
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-08-22 13:55:47 +0530

    react with cdn only

---
+++ app.js
@@ -0,0 +1,17 @@
+const heading = React.createElement("h1", { id: "heading" }, "Hello Nigga");
+
+//nested reactelements
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 nigga"),
+    React.createElement("h2", {}, "i am h2 nigga"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "i am h1 nigga"),
+    React.createElement("h2", {}, "i am h2 nigga"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
