14196a1c4686e25507533f554bc73011ade861f9
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-09-02 13:20:12 +0530

    used data from swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,35 +1,54 @@
-import resdata from "../utils/mockData";
 import Card from "./Card";
-import {useState} from "react";
-
+import { useState, useEffect } from "react";
 
 const Body = () => {
-    const[resname,setResName]=useState(
-        resdata
-    )
-    return (
-      <div className="body">
-        <div className="Filter">
-         <button className="filter-btn" onClick={()=>{
-            let top=4.5
-            const filteredData=resname.filter((res)=>
-                res.info.avgRating>=top
-            )
-            console.log(filteredData)
-            setResName(filteredData)
-         }}>Top Rated Restaurants</button>
-        </div>
-  
-        <div className="card-div">
-         {
-         resname.map((each)=>{
-          return <Card key={each.info.id} restrauntName={each.info.name} cloudinaryImageId={each.info.cloudinaryImageId} ratings={each.info.avgRating}/>
-         })
-        }
-        
-        </div>
-      </div>
+  const [resname, setResName] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.87560&lng=80.91150&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
+    const json=await data.json();
+    //console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    setResName(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
   };
 
-  export default Body;
+  return (
+    <div className="body">
+      <div className="Filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let top = 4.5;
+            const filteredData = resname.filter(
+              (res) => res.info.avgRating >= top
+            );
+            console.log(filteredData);
+            setResName(filteredData);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
+      <div className="card-div">
+        {resname.map((each) => {
+          return (
+            <Card
+              key={each.info.id}
+              restrauntName={each.info.name}
+              cloudinaryImageId={each.info.cloudinaryImageId}
+              ratings={each.info.avgRating}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
