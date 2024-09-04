bac21ff59122988b9de190bc293d820c6b0752f2
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-09-04 19:36:00 +0530

    Filtered_Button_integrated

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,6 +4,7 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [resname, setResName] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -13,19 +14,42 @@ const Body = () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.87560&lng=80.91150&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
-    const json=await data.json();
+    const json = await data.json();
     //console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
-    setResName(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setResName(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
-if(resname.length===0){
- return <Shimmer/>
-}
-
+  if (resname.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
       <div className="Filter">
+        <div className="search">
+          <input
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            className="search-btn"
+            onClick={() => {
+              console.log(searchText);
+              const filteredRes = resname.filter((res) => 
+                res.info.name.includes(searchText)
+              );
+              console.log(filteredRes);
+              setResName(filteredRes);
+            }}
+          >
+            search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
